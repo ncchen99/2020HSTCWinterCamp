c99f1e30743d09ba3216c82c0c872cca6b6599ac
小瓜呆黏誠 <ncchen99@gmail.com>
2020-01-19 20:59:10 +0800

    Add files via upload

--- HSTC2020/ex3.py
+++ HSTC2020/ex3.py
@@ -4,14 +4,14 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("星空")
 
-space = pygame.image.load("sky.JPG") #載入圖片
+space = pygame.image.load("sky.jpg") #載入圖片
 space.convert_alpha() 
  
 screen.blit(space, (0,0)) #放在視窗最左上角
 pygame.display.update()
-while True:
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    keys = pygame.key.get_pressed()
+            running = False
+pygame.quit()
